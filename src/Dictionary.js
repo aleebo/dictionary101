f60386f4e05315edfb84f0429eee008b57a44062
aleebo <87545942+aleebo@users.noreply.github.com>
2021-12-27 17:48:28 -0800

    Added photos and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "563492ad6f91700001000001777bd18ca31245a48a2d659c5052db12";
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
 
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
